4ca23d6fabbc852ded90a1ee9c177a5357376fa1
Shaun LaFleur <shaunlafleur.kj@gmail.com>
2016-12-10 15:54:53 -0600

    Added a solution to Problem 1 and Problem 2.

---
+++ Problem 1.js
@@ -0,0 +1,16 @@
+// Answer to this question: https://projecteuler.net/problem=1
+confirm("This script will find all of the multiples of two numbers chosen by the user that exist below another number chosen by the user.")
+var num1 = prompt("Enter the first number that you want to find the multiples of.");
+var num2 = prompt("Enter the second number that you want to find the multiples of.");
+var limit = prompt("You want to find all of the multiples BELOW what number? Note: The script will stop checking for multiples BEFORE this number.");
+var multiples = [];
+var multSum = 0;
+
+for(i=1; i<limit; i++) {
+  if(i % num1 === 0 || i % num2 === 0) {
+    multiples.push(i);
+    multSum += i;
+  }
+}
+console.log("These are the multiples of "+ num1 +" and "+ num2 +" below "+ limit +":"+ multiples);
+console.log("The sum of these numbers is "+ multSum);
---
+++ Problem 2.js
@@ -0,0 +1,43 @@
+// Customized answer to this problem: https://projecteuler.net/problem=2 
+confirm("This script will find the Fibonacci sequence starting with two numbers chosen by the user, whose values are less than a given limit chosen by the user.");
+var a = prompt("Please input the first number to start the sequence with.");
+var b = prompt("Please input the second number to start the sequence with.");
+var fibHolder = [parseInt(a),parseInt(b)];
+var limit = prompt("Please choose the limit for the sequence. The sequence will stop BEFORE this number.");
+var sumOfEven = 0;
+var sumTotal = fibHolder[0]+fibHolder[1];
+var i = 2;
+
+while(sumTotal < limit) { 
+  fibHolder.push(sumTotal);
+  if(sumTotal % 2 === 0) {
+    sumOfEven += sumTotal;
+  }
+  sumTotal = fibHolder[i]+fibHolder[i-1];
+  i++;
+}
+
+console.log("These are the values in the Fibonacci sequence starting with "+ a +" and "+ b + " whose values are less than "+ limit +": "+ fibHolder);
+console.log("The sum of only the even numbers within this sequence is: "+ sumOfEven);
+
+
+
+/* 
+// Simplified version
+
+var fibHolder = [1,2];
+var sumOfEven = 0;
+var sumTotal = 3;
+var i = 2;
+
+while(sumTotal < limit) { 
+  fibHolder.push(sumTotal);
+  if(sumTotal % 2 === 0) {
+    sumOfEven += sumTotal;
+  }
+  sumTotal = fibHolder[i]+fibHolder[i-1];
+  i++;
+}
+
+console.log(sumOfEven);
+*/
