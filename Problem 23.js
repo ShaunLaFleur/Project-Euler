35b630f09f7d7f8ce771944af7639440c6f5617e
Shaun K LaFleur <shaunlafleur.kj@gmail.com>
2016-12-12 18:58:20 -0600

    New idea for reducing calculations.

--- Problem 23.js
+++ Problem 23.js
@@ -8,6 +8,20 @@ A. Find all abundant numbers and push them into an array called abundant.
 	i, i is an abundant number.
 B. Create three nested for loops. First loop will use i(positive number to be checked), second will use j(number from abundant list) and third will use t(second number from abundant list)
 
+
+NEW METHOD: Create a two dimensional array. First row holds every number from 1 to 28123 and beneath each number we hold it's proper divisors.
+We can skip a ton of calculations by starting from 1, adding it as a proper divisor to each of it's 
+multiples(because all of it's multiples are properly divisible by 1). Then going to 2 and doing the same and so on and so forth. Once we have this 
+array/table built we can loop for each position and add up all of the values beneath it together to check if they sum to the position on the top row.
+
+
+for(i=1; i<28123; i++) {
+	for(j=i*2; j<28123; j=+i)
+	{push here}
+}
+
+
+
 */
 
 var abundant = [];
@@ -29,7 +43,6 @@ for(i=12; i<=28123; i++) {
 	}
 	divSum = 0;
 }
-
 
 // After finding all abundant numbers, this will check if i is a result of adding two of these abundant numbers
 // together. If it is not, it will be added to the sum. For some reason this method crashes if the limit for i is
