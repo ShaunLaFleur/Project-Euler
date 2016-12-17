81fc6624949970dcb273e10d79e88983d75b84f5
Shaun LaFleur <shaunlafleur.kj@gmail.com>
2016-12-16 20:52:44 -0600

    Made Project 3 more effecient.

--- Problem 3.js
+++ Problem 3.js
@@ -10,18 +10,12 @@ for(i=2; i<600851475143; i++) {
   if(600851475143 % i === 0) {
     var n = 600851475143/i;
     // Once the divisor is found, start checking if it's a prime number.
-    for(j=1; j<=n; j++) {
+    for(j=2; j<n; j++) {
       if(n % j === 0) {
-        counter++;
-      }
-      // If proper divisors are found other than 1 and itself, it's not a prime number so we break the loop here.
-      if(counter === 2 && j<n) {
-        n = 0;
-        counter = 0;
         break;
-        // Otherwise if j === n then the only proper divisors of n are 1 and n, so it is a prime number.
-      } else if(j === n) {
-        var highestPrime = n;
+      }
+      if(j == n-1) {
+        highestPrime = n;
       }
     }
   }
