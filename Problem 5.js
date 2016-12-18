348c867e2dc2dd2a61c6e1f85fbdc85cbf34640d
Shaun LaFleur <shaunlafleur.kj@gmail.com>
2016-12-17 18:36:09 -0600

    Optimization
    Optimized the calculations to increment i by 20 instead of 1, so we can
    skip all numbers not divisible by 20.

--- Problem 5.js
+++ Problem 5.js
@@ -1,3 +1,6 @@
+// Solution to this problem: https://projecteuler.net/problem=5
+// Since we want to get the smallest number evenly divisible by 1 through 20, we know that the number we're looking for MUST be divisible by 20, so we don't need to check any numbers NOT divisible by 20 and
+// can thus increment i by 20.
 var ourResult = 0;
 var i = 20;
 while(ourResult === 0) {
@@ -9,6 +12,6 @@ while(ourResult === 0) {
       ourResult = i;
     }
   }
-  i++
+  i+=20;
 }
 console.log(ourResult);
