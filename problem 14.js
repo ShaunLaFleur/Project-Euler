50b779fc6dff2bbf314e65e228732d71c48bc963
Shaun K LaFleur <shaunlafleur.kj@gmail.com>
2016-12-18 13:40:20 -0600

    Solved problem 14.

---
+++ problem 14.js
@@ -0,0 +1,23 @@
+var counter = 0;
+var j = 0;
+highestCount = 0;
+
+for(i=2; i<1000000; i++) {
+	j = i;
+	// Counter is 1, because i itself counts as part of the sequence.
+	counter = 1;
+	while(j !== 1) {
+		counter++;
+		if(j % 2 === 0) {
+			j = j/2;
+		} else if(j % 2 !== 0) {
+			j = (3*j)+1;
+		}
+		if(j === 1 && counter > highestCount) {
+			highestCount = counter;
+			highestNum = i;
+		}
+	}
+}
+console.log("The number with the longest sequence is "+highestNum+".");
+console.log("It's sequence is "+highestCount+" numbers long.");
