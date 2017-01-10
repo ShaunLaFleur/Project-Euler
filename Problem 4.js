c67cb2596709453a3087ec277cc04b9d0e9fcc5a
Shaun LaFleur <shaunlafleur.kj@gmail.com>
2017-01-09 21:30:03 -0600

    Optimized solution to problem 4.

--- Problem 4.js
+++ Problem 4.js
@@ -1,25 +1,17 @@
-var polyTest;
-var polyRev = [];
 var biggestPoly = 0;
-var n = 0;
-
 
 for(i=100; i<=999; i++) {
   for(j=i; j<=999; j++) {
-    n = i*j;
-    polyTest = n.toString();
-    // Push polyTest to an array in reverse.
-    for(a=polyTest.length-1; a>=0; a--) {
-      polyRev.push(polyTest[a]);
-    }
-    // Compare polyTest to polyRev and if it passes all, set n to biggestPoly if n > biggestPoly.
-    for(b=0; b<polyTest.length; b++) {
-      if(polyTest[b] !== polyRev[b]) {
-        polyRev = [];
+    var num = i*j;
+    var polyTest = num.toString();
+    for(n=0; n<=Math.round(polyTest.length/2); n++) {
+      if(polyTest[n] !== polyTest[polyTest.length-1-n]) {
         break;
-      }
-      if(b === polyTest.length-1 && n > biggestPoly) {
-      biggestPoly = n;
+      } else if(n === Math.round(polyTest.length/2)) {
+        polyTest = parseInt(polyTest);
+        if(polyTest > biggestPoly){
+          biggestPoly = num;
+        }
       }
     }
   }
