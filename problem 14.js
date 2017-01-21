9567b59f2984a091d7a1e4fcf133037f5f068b55
Shaun LaFleur <shaunlafleur.kj@gmail.com>
2017-01-20 23:20:07 -0600

    Replaced array with hash for problem 14.
    Using a hash is preferrable to using an array for problem 14 in order to
    store each number and it's related sequence length.

--- problem 14.js
+++ problem 14.js
@@ -1,6 +1,9 @@
+var t0 = performance.now();
+
 var counter = 0;
 var j = 0;
-highestCount = 0;
+var highestCount = 0;
+var shortCut = {};
 
 for(i=2; i<1000000; i++) {
 	j = i;
@@ -13,11 +16,18 @@ for(i=2; i<1000000; i++) {
 		} else {
 			j = (3*j)+1;
 		}
+		if(shortCut[j]) {
+		  counter += shortCut[j]-1;
+		  j = 1;
+		}
 		if(j === 1 && counter > highestCount) {
 			highestCount = counter;
-			highestNum = i;
+			var highestNum = i;
 		}
 	}
+	shortCut[i] = counter;
 }
 console.log("The number with the longest sequence is "+highestNum+".");
 console.log("It's sequence is "+highestCount+" numbers long.");
+var t1 = performance.now();
+console.log(t1-t0);
