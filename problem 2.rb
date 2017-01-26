04335dd5855acc1a729db704da011f6bbae47052
Shaun LaFleur <shaunlafleur.kj@gmail.com>
2017-01-26 14:36:50 -0600

    Optimizations

--- problem 2.rb
+++ problem 2.rb
@@ -1,24 +1,16 @@
-fib = [1, 1,]
+# Applied the shortcut found here: http://www.mathblog.dk/project-euler-problem-2/
+
+fib = [0, 2]
 i = 1
 limit = 4000000
-underlimit = false
 sum = 0
+n = 2
 
-while underlimit == false do
-	if fib[i] % 2 == 0 
-		sum += fib[i]
-	end
-
-	n = fib[i]+fib[i-1]
-
-	if n < limit 
-		fib.push(n)
-		i += 1
-	else
-		underlimit = true
-	end
+while n < limit do
+  fib[i] = n
+	sum += fib[i]
+	i += 1
+	n = 4*fib[i-1]+fib[i-2]
 end
 
 puts "The sum of all even numbers in the Fibonacci sequence is: #{sum}"
-puts "The sequence is: "
-puts fib
